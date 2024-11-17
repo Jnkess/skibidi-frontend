c3179a57107cf23db7c212a52f72c2e8b96691bd
Jakub Jankowski <jakub.jankowski20@gmail.com>
2024-11-18 00:40:53 +0100

    fix: remove .js files, omit creating .js durin type-check, feat: improve UI

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginRegisterView from '../views/LoginRegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'LoginRegister',
+      component: LoginRegisterView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/user',
+      name: 'user',
+      component: () => import('../views/UserView.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/AdminView.vue')
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: () => import('../views/MainPage.vue')
     }
   ]
 })
